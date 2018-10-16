0eb9accc6a722926c1750fa47557e4401e31f59a
MauroC97 <42740061+MauroC97@users.noreply.github.com>
2018-10-16 19:57:31 -0300

    Cambio la ubicacion donde se crean los archivos de los casos de prueba

--- src/mesadas/Test_resolucion.java
+++ src/mesadas/Test_resolucion.java
@@ -1,6 +1,6 @@
 package mesadas;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.junit.Assert;
@@ -9,39 +9,43 @@ import org.junit.jupiter.api.Test;
 class Test_resolucion {
 	
 	@Test
-	void caso1() throws FileNotFoundException {
-		Procesador_entrada procesador = new Procesador_entrada("caso1.in");
-		Generador_Salida generador = new Generador_Salida("salida.out");
+	void caso1() throws IOException {
+		Procesador_entrada procesador = new Procesador_entrada("Pruebas/caso1.in");
+		Generador_Salida generador = new Generador_Salida("Pruebas/salida1.out");
 		ArrayList<Mesada> arr = new ArrayList<Mesada>();
-		
+		int cant;
 		procesador.preparar_array(arr);
-		Assert.assertEquals(3, generador.cantidad_pilas(arr));
+		Assert.assertEquals(3, cant= generador.cantidad_pilas(arr));
+		generador.crear_arch_salida(cant);
 	}
 	@Test
-	void caso2() throws FileNotFoundException {
-		Procesador_entrada procesador = new Procesador_entrada("caso2.in");
-		Generador_Salida generador = new Generador_Salida("salida.out");
+	void caso2() throws IOException {
+		Procesador_entrada procesador = new Procesador_entrada("Pruebas/caso2.in");
+		Generador_Salida generador = new Generador_Salida("Pruebas/salida2.out");
 		ArrayList<Mesada> arr = new ArrayList<Mesada>();
-		
+		int cant;
 		procesador.preparar_array(arr);
-		Assert.assertEquals(4, generador.cantidad_pilas(arr));
+		Assert.assertEquals(4,cant= generador.cantidad_pilas(arr));
+		generador.crear_arch_salida(cant);
 	}
 	@Test
-	void caso3() throws FileNotFoundException {
-		Procesador_entrada procesador = new Procesador_entrada("caso3.in");
-		Generador_Salida generador = new Generador_Salida("salida.out");
+	void caso3() throws IOException {
+		Procesador_entrada procesador = new Procesador_entrada("Pruebas/caso3.in");
+		Generador_Salida generador = new Generador_Salida("Pruebas/salida3.out");
 		ArrayList<Mesada> arr = new ArrayList<Mesada>();
-		
+		int cant;
 		procesador.preparar_array(arr);
-		Assert.assertEquals(2, generador.cantidad_pilas(arr));
+		Assert.assertEquals(2, cant= generador.cantidad_pilas(arr));
+		generador.crear_arch_salida(cant);
 	}
 	@Test
-	void caso4() throws FileNotFoundException {
-		Procesador_entrada procesador = new Procesador_entrada("caso4.in");
-		Generador_Salida generador = new Generador_Salida("salida.out");
+	void caso4() throws IOException {
+		Procesador_entrada procesador = new Procesador_entrada("Pruebas/caso4.in");
+		Generador_Salida generador = new Generador_Salida("Pruebas/salida4.out");
 		ArrayList<Mesada> arr = new ArrayList<Mesada>();
-		
+		int cant;
 		procesador.preparar_array(arr);
-		Assert.assertEquals(2, generador.cantidad_pilas(arr));
+		Assert.assertEquals(2, cant=generador.cantidad_pilas(arr));
+		generador.crear_arch_salida(cant);
 	}
 }
